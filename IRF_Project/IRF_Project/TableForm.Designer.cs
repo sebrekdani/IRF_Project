447db7695dc9690b742775b9d1c7a340ff393037
Daniel Sebrek <dani.sebrek@gmail.com>
2020-12-13 19:01:33 +0100

    created function to export to excel

--- IRF_Project/IRF_Project/TableForm.Designer.cs
+++ IRF_Project/IRF_Project/TableForm.Designer.cs
@@ -37,7 +37,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 12);
+            this.label1.Location = new System.Drawing.Point(21, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 7;
--- IRF_Project/IRF_Project/TableForm.cs
+++ IRF_Project/IRF_Project/TableForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace IRF_Project
 {
@@ -16,6 +18,24 @@ namespace IRF_Project
         List<Player> playersToFilter = new List<Player>();
         string _choosenPost;
         Form1 _mainForm;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+        string[] headers = new string[]
+        {
+            "Name",
+            "Year",
+            "Month",
+            "Day",
+            "Post",
+            "Goal",
+            "Assist",
+            "Yellow Card",
+            "Red Card",
+            "Average"
+        };
+
         public TableForm(Form1 mainForm, List<Player> players, List<string> postList)
         {
             InitializeComponent();
@@ -45,7 +65,99 @@ namespace IRF_Project
 
         private void ExportBT_Click(object sender, EventArgs e)
         {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to export \n the list of not injured players ordered by 'Average' to excel?","Exporting to excel...",MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
             List<Player> exportList = CreateNotInjuredList();
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable(exportList);
+                FormatTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+            return ExcelCoordinate;
+        }
+
+        private void CreateTable(List<Player> exportList)
+        {
+            object[,] values = new object[exportList.Count, headers.Length];
+            for (int col = 1; col <= headers.Length; col++)
+            {
+                xlSheet.Cells[1, col] = headers[col-1];
+            }
+            int counter = 0;
+            foreach (Player player in exportList)
+            {
+                values[counter, 0] = player.Name;
+                values[counter, 1] = player.Year;
+                values[counter, 2] = player.Month;
+                values[counter, 3] = player.Day;
+                values[counter, 4] = player.Post;
+                values[counter, 5] = player.Goal;
+                values[counter, 6] = player.Assist;
+                values[counter, 7] = player.YellowCard;
+                values[counter, 8] = player.RedCard;
+                values[counter, 9] = player.Average;
+                counter++;
+                xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            }
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.FromArgb(255, 66, 66);
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, headers.Length));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColRange = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+            firstColRange.Font.Bold = true;
+            firstColRange.Interior.Color = Color.FromArgb(252, 255, 66);
+
+            Excel.Range centerColsRange = xlSheet.get_Range(GetCell(2, 6), GetCell(lastRowID, 9));
+            centerColsRange.HorizontalAlignment= Excel.XlHAlign.xlHAlignCenter;
+
+            Excel.Range lastColRange = xlSheet.get_Range(GetCell(2, headers.Length), GetCell(lastRowID, headers.Length));
+            lastColRange.Interior.Color = Color.FromArgb(252, 255, 66);
+            lastColRange.NumberFormat = "?.??";
         }
 
         private void FillDataGridView(List<Player> players)
@@ -61,7 +173,6 @@ namespace IRF_Project
                                 select x).Distinct();
             postsComboBox.DataSource = postDistinct.ToList();
         }
-
 
         private List<Player> CreateNotInjuredList()
         {
